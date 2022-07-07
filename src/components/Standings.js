06c28fd2da3f0d8061cb95dc4fa713dd8ffffe2c
Fahad Guled <fahad.guled@yahoo.com>
2022-07-07 00:16:44 -0700

    created new state to store team information for standings. Also added tie breaker condition in sort method. Displayed team info into standings table

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,62 +1,78 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import sleeper from "../api/sleeper";
 
 // Using a Map instead of an Array to help us look up the team owners information faster
-const teamData = new Map();
-
-const getStandings = async () => {
-  const response = await sleeper.get("/league/845531683540303872/rosters");
-  console.log(response.data);
-  for (let i = 0; i < response.data.length; i++) {
-    // Key = owner id, value is an object that stores wins, losses, ties and total fantasy points scored, we can add more values like divisions etc as we go along
-    // Changed the value of wins for testing since the season didn't begin yet. We'll set it equal to response.data[i].settings.wins once we have data to work with
-    teamData.set(response.data[i].owner_id, {
-      wins: i + 1,
-      losses: response.data[i].settings.losses,
-      ties: response.data[i].settings.ties,
-      fantasy_points: response.data[i].settings.fpts,
-    });
-  }
-  // Sorted the Map by wins
-  const sortedTeamData = new Map(
-    [...teamData.entries()].sort((a, b) => b[1]["wins"] - a[1]["wins"])
-  );
-  console.log(teamData.get("844649764703297536")["wins"]);
-};
 
 const Standings = () => {
+  const [teams, setTeams] = useState([]);
+
+  let changed = false;
+  if (teams.length > 0) {
+    changed = true;
+  }
+  const teamData = new Map();
+
+  const getStandings = async () => {
+    const response = await sleeper.get("/league/845531683540303872/rosters");
+    setTeams(response.data);
+    console.log(teams);
+    console.log(response.data);
+
+    for (let i = 0; i < response.data.length; i++) {
+      // Key = owner id, value is an object that stores wins, losses, ties and total fantasy points scored, we can add more values like divisions etc as we go along
+      // Changed the value of wins for testing since the season didn't begin yet. We'll set it equal to response.data[i].settings.wins once we have data to work with
+      teamData.set(response.data[i].owner_id, {
+        wins: 1,
+        losses: response.data[i].settings.losses,
+        ties: response.data[i].settings.ties,
+        fantasy_points: 30 + i + response.data[i].settings.fpts,
+      });
+    }
+
+    // Sorted the Map by wins
+    const sortedTeamData = new Map(
+      [...teamData.entries()].sort(
+        (a, b) =>
+          b[1]["wins"] -
+          a[1]["wins"] +
+          (b[1]["fantasy_points"] - a[1]["fantasy_points"])
+      )
+    );
+
+    console.log(sortedTeamData);
+  };
+
   useEffect(() => {
     getStandings();
-  }, []);
+  }, [changed]);
 
-  const standings = (
-    <div className=" flex justify-center pt-[50px]">
-      <table className="table-fixed bg-[red]">
-        <thead>
-          <tr>
-            <th>Team</th>
-            <th>Wins</th>
-            <th>Losses</th>
-            <th>Ties</th>
-            <th>Fantasy Points</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td className="team">{teamData[0]}</td>
-            <td className="wins">
-              {teamData.get("844649764703297536")["wins"]}
-            </td>
-            <td className="losses">1961</td>
-            <td className="ties">1961</td>
-            <td className="fantasypoints">1961</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
-
-  return <div>{!teamData ? "loading..." : standings}</div>;
+  const standings = teams.map((team) => {
+    return (
+      <div className=" flex justify-center pt-[50px]">
+        <table className="table-fixed bg-[red]">
+          <thead>
+            <tr>
+              <th>Team</th>
+              <th>Wins</th>
+              <th>Losses</th>
+              <th>Ties</th>
+              <th>Fantasy Points</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="team">{team.owner_id}</td>
+              <td className="wins">{team.settings.wins}</td>
+              <td className="losses">{team.settings.losses}</td>
+              <td className="ties">{team.settings.ties}</td>
+              <td className="fantasypoints">{team.settings.fpts}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  });
+  return <div>{standings}</div>;
 };
 
 export default Standings;
