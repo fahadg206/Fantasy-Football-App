28c84cd54f09c6c3ea34e2340a0d010e20872c90
Fahad Guled <fahad.guled@yahoo.com>
2022-07-14 17:51:59 -0700

    added template for articles in articles component

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Articles = () => {
-  return <div>Articles</div>;
+  return (
+    <div className="flex flex-col justify-center items-center pt-[100px]">
+      <ul className=" flex flex-col justify-center bg-[#1a1a1c] w-[20vw] h-[30vh] text-[white] text-center rounded-[30px]">
+        <li>fdsfsdfddfsfssfdfdssdsdffsd</li>
+        <li>fdsfsdfddfsfssfdfdssdsdffsd</li>
+        <li>fdsfsdfddfsfssfdfdssdsdffsd</li>
+        <li>fdsfsdfddfsfssfdfdssdsdffsd</li>
+        <li>fdsfsdfddfsfssfdfdssdsdffsd</li>
+        <li>fdsfsdfddfsfssfdfdssdsdffsd</li>
+        <button className="text-white text-center bg-[#1a1a1c] group border-2 px-6 py-3 my-2 flex justify-center hover:bg-[#ba30e8] hover:border-[#ba30e8]">
+          View Work
+          <span className="group-hover:rotate-90 duration=300">
+            <HiArrowNarrowRight className="ml-2" />
+          </span>
+        </button>
+      </ul>
+    </div>
+  );
 };
 
 export default Articles;
