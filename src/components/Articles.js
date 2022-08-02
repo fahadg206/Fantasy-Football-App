92d5af763e4d2741836d773dd1ed01ff3c39c087
Fahad Guled <fahad.guled@yahoo.com>
2022-08-02 01:48:06 -0700

    styled article page with boiler plate information

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -33,9 +33,9 @@ const Articles = () => {
     );
   };
   return (
-    <div>
-      <div className="hidden sm:flex justify-center items-center pt-[100px] ml-[20px] gap-[50px] ">
-        <div className="flex flex-col items-center justify-center text-center polls text-[white] p-[20px] rounded-[40px] bg-[#1A4AAC] ">
+    <div className="min-h-screen bg-[url('file:///C:/Users/fahad/OneDrive/Desktop/portfolio/src/images/wesbite%20background.png')]">
+      <div className="hidden sm:grid grid-cols-4 justify-items-center grid-rows-2">
+        <div className="flex flex-col items-center justify-center text-center polls text-[black] rounded-[10px] bg-[#eaecee] h-[60%] p-[10px] shadow-md shadow-black">
           <h2 className="mb-[15px] text-2xl">Weekly Articles</h2>
           <ul className="text-[17px]">
             <li className="p-[5px]">
@@ -58,8 +58,31 @@ const Articles = () => {
             </li>
           </ul>
         </div>
-        <div className="w-[60%]">
+        <div className="col-span-2 justify-self-start">
           <Week2 />
+        </div>
+        <div className="flex flex-col items-center justify-center text-center polls text-[black] rounded-[10px] bg-[#eaecee] h-[60%] p-[10px] shadow-md shadow-black">
+          <h2 className="mb-[15px] text-2xl">Headlines</h2>
+          <ul className="text-[17px]">
+            <li className="p-[5px]">
+              <Link to="/articles/week1">Week1</Link>
+            </li>
+            <li className="p-[5px]">
+              <Link to="/articles/week1">Week1</Link>
+            </li>
+            <li className="p-[5px]">
+              <Link to="/articles/week1">Week1</Link>
+            </li>
+            <li className="p-[5px]">
+              <Link to="/articles/week1">Week1</Link>
+            </li>
+            <li className="p-[5px]">
+              <Link to="/articles/week1">Week1</Link>
+            </li>
+            <li className="p-[5px]">
+              <Link to="/articles/week1">Week1</Link>
+            </li>
+          </ul>
         </div>
       </div>
 
--- src/components/WeeklyArticles/Week2.js
+++ src/components/WeeklyArticles/Week2.js
@@ -2,45 +2,37 @@ import React from "react";
 
 const Week2 = () => {
   return (
-    <div>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-      minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-      ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-      est laborum.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-      do
+    <div className="grid grid-cols-2 gap-x-24 bg-white rounded-[10px] p-[25px]">
+      <div className="">
+        <img
+          className="rounded-[10px]"
+          src="https://a57.foxsports.com/statics.foxsports.com/www.foxsports.com/content/uploads/2022/08/364/607/08.01.22_GoodellWatson_Vertical.jpg?ve=1&tl=1"
+        />
+      </div>
+      <div className="grid grid-cols-1 gap-y-3">
+        <p className="text-6xl text-bold">DESHAUN BEATS THE CASE</p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum
+          dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+          incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+          commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+          occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+          mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+          ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+          irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+          fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        </p>
+      </div>
     </div>
   );
 };
