c746227538a383c9bebbc156cbfea5b4f6dd3cfa
Kaboweyne <mahad.a.fahiye@gmail.com>
2022-08-03 22:41:31 -0500

    resolved merge conflicts

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -32,7 +32,7 @@ const Articles = () => {
     );
   };
   return (
-    <div className="min-h-screen bg-[url('file:///C:/Users/fahad/OneDrive/Desktop/portfolio/src/images/wesbite%20background.png')]">
+    <div className="min-h-screen bg-[url('/src/images/bgpng.png')]">
       <div className="flex flex-col md:grid grid-cols-4 justify-items-center">
         <div className="hidden md:flex flex-col items-center justify-center text-center polls text-[black] rounded-[10px] bg-[#eaecee] h-[400px] p-[10px] shadow-md shadow-black sticky top-0">
           <h2 className="mb-[15px] text-2xl">Weekly Articles</h2>
--- src/components/PowerRankings.js
+++ src/components/PowerRankings.js
@@ -4,7 +4,9 @@ const PowerRankings = () => {
   return (
     <div className="grid grid-cols-1 items-center justify-items-center font-bold text-6xl">
       <div className="h-screen">PowerRankings</div>
-      <div className="h-screen">Number 10</div>
+      <div className=" h-screen w-full bg-[url('/src/images/Jewels_bounty.png')] bg-auto bg-no-repeat ">
+        Number 10
+      </div>
       <div className="h-screen">Number 9</div>
       <div className="h-screen">Number 8</div>
     </div>
