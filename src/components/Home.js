f3e0d155f53a01de40e6f29b8db575b32519154d
Fahad Guled <fahad.guled@yahoo.com>
2022-09-12 00:09:50 -0700

    testing change

--- src/components/Home.js
+++ src/components/Home.js
@@ -81,9 +81,9 @@ const Home = () => {
         <div className="flex justify-center w-[90%] ml-5 col-span-2 self-center">
           <TrendingAdds />
         </div>
-        <div className="col-span-2 rounded-[10px] self-center lg: lg:mx-auto col-start-3  shadow-lg shadow-black">
+        {/* <div className="col-span-2 rounded-[10px] self-center lg: lg:mx-auto col-start-3  shadow-lg shadow-black">
           <HighestScorer />
-        </div>
+        </div> */}
         <div className="col-span-2 flex justify-center w-[90%] self-center">
           <TrendingDrops />
         </div>
