ecc41a4b8a903e0b89cc6fd0facb515a4b6206d9
Fahad Guled <fahad.guled@yahoo.com>
2022-09-13 21:25:26 -0700

    changed weekly matchups

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -35,7 +35,7 @@ const Layout = ({ children }) => {
 
   const getSchedule = async () => {
     const response = await sleeper.get(
-      `league/${REACT_APP_LEAGUE_ID}/matchups/1`
+      `league/${REACT_APP_LEAGUE_ID}/matchups/2`
     );
     setSchedule(response.data);
   };
