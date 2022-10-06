b47d4a7e2f19585cc4f8a19e0ca728409b616073
Fahad Guled <fahad.guled@yahoo.com>
2022-10-06 09:41:09 -0700

    week 5 updates to scores, schedule, and highest scorers

--- src/components/HighestScorer.js
+++ src/components/HighestScorer.js
@@ -21,7 +21,7 @@ const HighestScorer = () => {
   const getSchedule = async () => {
     //returns roster id & matchup id
     const response = await sleeper.get(
-      `league/${REACT_APP_LEAGUE_ID}/matchups/4`
+      `league/${REACT_APP_LEAGUE_ID}/matchups/5`
     );
     setSchedule(response.data);
   };
--- src/components/Redemption/TopScorerRL.js
+++ src/components/Redemption/TopScorerRL.js
@@ -22,7 +22,7 @@ const TopScorerRL = () => {
   const getSchedule = async () => {
     //returns roster id & matchup id
     const response = await sleeper.get(
-      `league/${REACT_APP_REDEMPTION_LEAGUE_ID}/matchups/4`
+      `league/${REACT_APP_REDEMPTION_LEAGUE_ID}/matchups/5`
     );
     setSchedule(response.data);
   };
