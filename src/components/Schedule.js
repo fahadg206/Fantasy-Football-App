cd4f22d9f53267dabe02c99178efbd2982b00745
Fahad Guled <fahad.guled@yahoo.com>
2022-11-17 22:26:46 -0800

    schedule for week 11

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -30,7 +30,7 @@ const Schedule = () => {
   const getSchedule = async () => {
     //returns roster id & matchup id
     const response = await sleeper.get(
-      `league/${REACT_APP_LEAGUE_ID}/matchups/10`
+      `league/${REACT_APP_LEAGUE_ID}/matchups/11`
     );
     setSchedule(response.data);
   };
